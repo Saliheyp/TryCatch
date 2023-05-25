1fb1d483349c85c1debac5365cd7d1e0c423b975
Saliheyp <m.saliheyupoglu@gmail.com>
2023-05-25 17:41:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace egitimTry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Bir sayi giriniz");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi:"+sayi);
+            }
+            catch(Exception ex)
+            {
+                 Console.WriteLine("Hata" + ex.Message.ToString());
+
+            }
+            //finally
+            //{
+              //  Console.WriteLine("İşlem tamamlandı.");
+                
+            //}
+            try
+            {
+                int a = int.Parse("A");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş Değer Girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);
+                
+            }
+            
+                  
+        }
+    }
+}
